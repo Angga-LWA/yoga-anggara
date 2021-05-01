ba53cae8fe2ea869890f57e1d4cb96ec71d567d6
Angga-LWA <69418956+Angga-LWA@users.noreply.github.com>
2021-05-02 00:25:15 +0700

    Update kategory.py
    Finish kategory.py

--- backend/modules/module_kategori/kategori.py
+++ backend/modules/module_kategori/kategori.py
@@ -156,4 +156,43 @@ class post:
     def __init__(self):
         pass
 
-    def
+    def add_data(self):
+        response = {
+                        "message"       : {},
+                        "data"          : {}
+                   }
+        try:
+            nama_kategori = request.json.get('nama_kategori')
+            tgl_kategori  = request.json.get('tgl_kategori')
+            flag_kategori = request.json.get('flag_kategori')
+
+            tgl_kategori  = datetime.datetime.strptime(tgl_kategori, '%d-%m-%Y')
+
+            get_data      = Kategori.query.filter_by(nama_kategori=nama_kategori).first()
+            if get_data is not None:
+                response["message"]     = 'Failed'
+                response["data"]        = 'Nama Kategori Already Exist!'
+                return make_response(json.dumps(response)), 400
+
+            kategori = Kategori(nama_kategori=nama_kategori, tgl_kategori=tgl_kategori, flag_kategori=flag_kategori)
+            db.session.add(kategori)
+            db.session.commit()
+
+            tgl_log = datetime.datetime.now()
+            log = 'Menambahkan Data Kategori Terbaru'
+
+            log = Log(tgl_log=tgl_log, log=log, username_in=username_in)
+            db.session.add(log)
+            db.session.commit()
+
+            response["message"]     = 'Success'
+            response["data"]        = "data berhasil masuk"
+
+            return make_response(json.dumps(response)), 200
+
+        except:
+
+            response["message"]     = 'Failed'
+            response["data"]        = "Error"
+
+            return make_response(json.dumps(response)), 400
